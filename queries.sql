f8579816a1f54016e562cbf651be760bf7825908
stino-x <107426247+stino-x@users.noreply.github.com>
2023-09-30 18:09:46 +0200

    Update data.sql and queries.sql  i also included the step where they said we should make the species unspecified as i did it already i just forgot to attach the image earlier

--- queries.sql
+++ queries.sql
@@ -25,7 +25,8 @@ SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
 --- Task 2 ----
-
+UPDATE animals
+SET species = 'unspecified';
 -- Start a transaction
 BEGIN TRANSACTION;
 
