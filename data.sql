af4f7faa807c1dfc89111cf886df7b505c3f3320
stino-x <107426247+stino-x@users.noreply.github.com>
2023-09-30 11:04:59 +0200

    Task 2:
    1. Added several animals with negative weights to the database.
    2. Updated animals with names ending in "mon" to have species as "digimon".
    3. Updated animals without a species to have species as "pokemon".
    4. Deleted rows where the name is an empty string or NULL.
    5. Started a transaction, deleted all records in the animals table, and rolled back the transaction.
    6. Verified if all records in the animals table still exist.
    7. Started a transaction, deleted all animals born after Jan 1st, 2022, created a savepoint, updated all animals' weight to be their weight multiplied by -1, rolled back to the savepoint, and updated all animals' weights that are negative to be their weight multiplied by -1.
    8. Committed the transaction. (generated by useblackbox.ai )

--- data.sql
+++ data.sql
@@ -16,3 +16,47 @@ VALUES ('Pikachu', '2021-01-07', 15.04, false, 1);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
 VALUES ('Devimon', '2017-05-12', 11.0, true, 5);
 
+---- TASK 2-------
+
+-- Inserting the first animal (Charmander) with a negative weight
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
+VALUES ('Charmander', '2020-02-08', -11.0, false, 0, 'Fire-type');
+
+-- Inserting the second animal (Plantmon) with a negative weight
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
+VALUES ('Plantmon', '2021-11-15', -5.7, true, 2, 'Grass-type');
+
+-- Inserting the third animal (Squirtle) with a negative weight
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
+VALUES ('Squirtle', '1993-04-02', -12.13, false, 3, 'Water-type');
+
+-- Inserting the fourth animal (Angemon) with a negative weight
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
+VALUES ('Angemon', '2005-06-12', -45.0, true, 1, 'Holy-type');
+
+-- Inserting the fifth animal (Boarmon)
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
+VALUES ('Boarmon', '2005-06-07', 20.4, true, 7, 'Ground-type');
+
+-- Inserting the sixth animal (Blossom)
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
+VALUES ('Blossom', '1998-10-13', 17.0, true, 3, 'Grass-type');
+
+-- Inserting the seventh animal (Ditto)
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts, species)
+VALUES ('Ditto', '2022-05-14', 22.0, true, 4, 'Normal-type');
+
+-- c:\Users\Danskids\Downloads\Screenshot 2023-09-30 101448.png
+-- c:\Users\Danskids\Downloads\Screenshot 2023-09-30 101524.png
+-- c:\Users\Danskids\Downloads\Screenshot 2023-09-30 101556.png
+
+-- c:\Users\Danskids\Downloads\Screenshot 2023-09-30 103209.png
+-- c:\Users\Danskids\Downloads\Screenshot 2023-09-30 102931.png
+-- c:\Users\Danskids\Downloads\Screenshot 2023-09-30 103024.png
+
+-- c:\Users\Danskids\Downloads\Screenshot 2023-09-30 103520.png
+
+-- c:\Users\Danskids\Downloads\Screenshot 2023-09-30 104118.png
+
+-- c:\Users\Danskids\Downloads\Screenshot 2023-09-30 105353.png
+-- c:\Users\Danskids\Downloads\Screenshot 2023-09-30 105441.png
--- queries.sql
+++ queries.sql
@@ -24,4 +24,92 @@ SELECT * FROM animals WHERE name <> 'Gabumon';
 -- 8. Find all animals with a weight between 10.4kg and 17.3kg (inclusive)
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
+--- Task 2 ----
+
+-- Start a transaction
+BEGIN TRANSACTION;
+
+-- Update animals with names ending in "mon" to have species as "digimon"
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+-- Update animals without a species to have species as "pokemon"
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+-- Commit the transaction to save the changes
+COMMIT;
+
+-- Delete rows where the name is an empty string or NULL
+DELETE FROM animals
+WHERE name = '' OR name IS NULL;
+
+-- Start a transaction
+BEGIN TRANSACTION;
+
+-- Delete all records in the animals table
+DELETE FROM animals;
+
+-- Roll back the transaction (undo the delete operation)
+ROLLBACK;
+
+-- Verify if all records in the animals table still exist
+SELECT * FROM animals;
+
+-- Start a transaction
+BEGIN TRANSACTION;
+
+-- Delete all animals born after Jan 1st, 2022
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+-- Create a savepoint for the transaction
+SAVEPOINT my_savepoint;
+
+-- Update all animals' weight to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+-- Rollback to the savepoint
+ROLLBACK TO my_savepoint;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+-- Commit the transaction
+COMMIT;
+
+-- How many animals are there?
+SELECT COUNT(*) AS total_animals
+FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT COUNT(*) AS no_escape_animals
+FROM animals
+WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) AS average_weight
+FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, MAX(escape_attempts) AS max_escape_attempts
+FROM animals
+GROUP BY neutered;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
+FROM animals
+GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
+
 
