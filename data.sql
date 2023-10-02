5f2dca045950ec5d2a15785f44fccb8f5ace4ad1
stino-x <107426247+stino-x@users.noreply.github.com>
2023-10-02 18:07:17 +0200

    Add owners and species tables and update animals table to include species_id (generated by useblackbox.ai )

--- data.sql
+++ data.sql
@@ -48,6 +48,21 @@ VALUES ('Ditto', 7, '2022-05-14', 22.0, true, 4, 'Normal-type');
 
 --- Task-4 ----
 
+-- Insert data into the "owners" table
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+       ('Jennifer Orwell', 19),
+       ('Bob', 45),
+       ('Melody Pond', 77),
+       ('Dean Winchester', 14),
+       ('Jodie Whittaker', 38);
+
+-- Insert data into the "species" table
+INSERT INTO species (name)
+VALUES ('Pokemon'),
+       ('Digimon');
+
+
 -- Update animals to include species_id based on name
 UPDATE animals
 SET species_id = (CASE
